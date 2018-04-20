d7c9510795bbf8f4f1735c45066db399fdb3fd2e
Carsten Munk <carsten.munk@zipperglobal.com>
2018-04-20 05:10:14 +0200

    Bit better imports
    Signed-off-by: Carsten Munk <carsten.munk@zipperglobal.com>

--- src/main.js
+++ src/main.js
@@ -1,22 +1,5 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-
-var vault_opts = {}
-console.log('hash is ' + location.hash)
-if (location.hash.startsWith('#zippie-vault=')) {
-  vault_opts = {
-    'vaultURL': location.hash.slice('#zippie-vault='.length)
-  }
-  location.hash = ''
-}
-/* vue idiocy */
-if (location.hash.startsWith('#/zippie-vault=')) {
-  vault_opts = {
-    'vaultURL': location.hash.slice('#/zippie-vault='.length)
-  }
-  location.hash = ''
-}
-
 import Vue from 'vue'
 import App from './App'
 import Web3 from 'web3'
@@ -25,6 +8,24 @@ import router from './router'
 var zippieprovider = require('vault-web3-provider')
 var vault = require('vault-api')
 var vaultSecp256k1 = require('vault-api/src/secp256k1.js')
+
+var vault_opts = {}
+
+/* This handles being launched by vault. */
+if (location.hash.startsWith('#zippie-vault=')) {
+  vault_opts = {
+    'vaultURL': location.hash.slice('#zippie-vault='.length)
+  }
+  location.hash = ''
+}
+/* And this handles the terrifying fact that Vue automatically converts #foo to #/foo upon loading router with hash option */
+if (location.hash.startsWith('#/zippie-vault=')) {
+  vault_opts = {
+    'vaultURL': location.hash.slice('#/zippie-vault='.length)
+  }
+  location.hash = ''
+}
+
 
   
 //Vue.config.productionTip = false
